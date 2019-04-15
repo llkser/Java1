0f0f73ebd0c91f8cb8de9e625bb3e2b13dc18efb
Kang Liu <sc17kl@leeds.ac.uk>
2019-04-15 23:24:16 +0800

    TrackInfo

--- src/TrackInfo.java
+++ src/TrackInfo.java
@@ -4,7 +4,7 @@ public class TrackInfo {
 	public static void main(String[] args) {
 		if(args.length == 0){  
 			System.out.println("No File to be Read!");  
-			System.exit(-1);
+			System.exit(1);
 	    } 
 		Track track1=new Track();
 		try {
@@ -12,6 +12,15 @@ public class TrackInfo {
 		}
 		catch (FileNotFoundException e) {
 			e.getMessage();
+			System.exit(1);
 		}
+		catch (GPSException e) {
+			System.exit(1);
+		}
+		System.out.printf("%d points in track\n",track1.size());
+		System.out.printf("Lowest point is (%f, %f), %.1f m\n",track1.lowestPoint().getLongitude(),track1.lowestPoint().getLatitude(),track1.lowestPoint().getElevation());
+		System.out.printf("Highest point is (%f, %f), %.1f m\n",track1.highestPoint().getLongitude(),track1.highestPoint().getLatitude(),track1.highestPoint().getElevation());
+		System.out.printf("Total distance = %.3f km\n",track1.totalDistance()/1000);
+		System.out.printf("Average speed = %.3f m/s\n",track1.averageSpeed());
 	}
 }
