c33251cbef91be9d09de6c774c3a53593dce61d5
Kang Liu <sc17kl@leeds.ac.uk>
2019-04-15 19:16:20 +0800

    basictests

--- src/Track.java
+++ src/Track.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
@@ -12,12 +13,43 @@ public class Track{
 	public Track() {
 		tr=new ArrayList<Point>();
 	}
-	public void readFile(String str) {
-		try {
-			Scanner scanner= new Scanner(new File(str));
-		} catch (FileNotFoundException e) {
-			
-		}
+	public void readFile(String str) throws FileNotFoundException{
+			if(this.size()>0)
+				tr.clear();
+			Scanner sc= new Scanner(new File(str));
+			Scanner vScanner= null;
+			sc.nextLine();
+			int index;
+			String s;
+			double a,b,c;
+			ZonedDateTime data=null;
+			a=b=c=0;
+			Point p;
+			while(sc.hasNext())
+			{
+				vScanner=new Scanner(sc.nextLine());
+				vScanner.useDelimiter(",");
+				index=0;
+				while(vScanner.hasNext())
+				{
+					s=vScanner.next();
+					if(index==0)
+						data=ZonedDateTime.parse(s);
+					else if(index==1)
+						a=Double.parseDouble(s);
+					else if(index==2)
+						b=Double.parseDouble(s);
+					else if(index==3)
+					{
+						c=Double.parseDouble(s);
+						p=new Point(data, a, b, c);
+						tr.add(p);
+					}
+					index++;
+					if(index==4)
+						index=0;
+				}
+			}
 	}
 	public void add(Point a) {
 		tr.add(a);
